90af7e6e278117d9703beedb10f0c1267da8259e
unknown <aswar.my123@gmail.com>
2026-05-29 16:30:39 +0800

    pwa 3

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = "my-pwa-cache-v1";
+
+self.addEventListener("install", event => {
+self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request).then(response => {
+return response || fetch(event.request);
+})
+);
 });
